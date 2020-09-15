30dee9e896fee1387e4cd6c22172699703e86a08
mcdarsenekmwale <mcdonaldkmwale@gmail.com>
2020-09-15 16:28:48 +0800

    device orientation changed to a static style

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:travelapp/screens/welcome_screen.dart';
 import 'package:travelapp/utils/app_constants.dart';
 
 void main() {
-  runApp(
-     MaterialApp(
-       debugShowCheckedModeBanner: false,
-        title: 'travelApp',
-        theme: AppConstants.appTheme,
-        home: WelcomeScreen(),
-    )
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown
+      ]
+  ).then((_) =>
+      runApp(
+          MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'travelApp',
+            theme: AppConstants.appTheme,
+            home: WelcomeScreen(),
+          )
+      )
   );
 }
